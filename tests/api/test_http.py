a5df0682a69afc9b0d51bc38fcf2c156154086c8
Наталія Познанська <poznanskyi.d@gmail.com>
2024-02-24 23:50:21 +0200

    Fourth task

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -4,23 +4,23 @@ import requests
 
 @pytest.mark.http
 def test_first_request():
-    r = requests.get('https://api.github.com/zen')
+    r = requests.get("https://api.github.com/zen")
     print(f"Response: {r.text}")
 
 
 @pytest.mark.http
 def test_second_request():
-    r = requests.get('https://api.github.com/users/Poznanskyi')
+    r = requests.get("https://api.github.com/users/Poznanskyi")
     body = r.json()
     headers = r.headers
 
     assert body['name'] == 'Dmytro'
     assert r.status_code == 200
-    assert headers['Server'] == 'GitHub.com'
+    assert headers['Server'] == "GitHub.com"
 
 
 @pytest.mark.http
 def test_status_code_request():
-    r = requests.get('https://api.github.com/users/sergii_butenko')
+    r = requests.get("https://api.github.com/users/sergii_butenko")
 
     assert r.status_code == 404
